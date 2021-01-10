8469ca0fd03ca3d54606b7c6ef8cc320dc07aa25
zack dove <zdove@live.co.uk>
2021-01-10 13:26:55 +0000

    Adding font + finishing up on JS

--- main.js
+++ main.js
@@ -1,152 +1,155 @@
-//all js code just draw circles ,svg filter is important code
-
 const canvas = document.getElementById("canvas"),
-  context = canvas.getContext("2d"),
-  colorPallete =
-      // ["#00f", "#00a", "#00b", "#00c", "#00d", "#00e"];
-      ["rgb(230, 0, 100)"]
-// ["#f00","#a00","#b00","#c00","#d00","#e00"];
-// ["white","#888","yellow","orange","darkorange","darkmagenta","darkgreen","khaki"];
-  bluePallete = ["rgb(0,0,255)"]
+context = canvas.getContext("2d"),
+redPallete = ["rgb(230, 0, 100)"]
+bluePallete = ["rgb(0,0,255)"]
 
 var width = canvas.width = window.innerWidth,
-  height = canvas.height = window.innerHeight,
-  src = {
-    x: width / 2,
-    y: height / 2
-  },
-  circles = [];
-  blues = [];
+height = canvas.height = window.innerHeight,
+src = {
+	x: width / 2,
+	y: height / 2
+},
+reds = [];
+blues = [];
 
 window.onresize = function() {
-  width = canvas.width = window.innerWidth;
-  height = canvas.height = window.innerHeight;
-  src.x= width / 2;
-  src.y= height / 2;
+	width = canvas.width = window.innerWidth;
+	height = canvas.height = window.innerHeight;
+	src.x= width / 2;
+	src.y= height / 2;
 }
 
-class Circle {
-  constructor() {
-    this.x = src.x * 2  +100;
-    this.y = (src.y / 2) + src.y + ((Math.random()-0.5)*src.y);
-      this.angle = Math.PI + ((Math.random()-0.5)*0.2);
-    var speed=1 + Math.random();
-    this.vx = speed* Math.cos(this.angle);
-    this.vy = speed* Math.sin(this.angle);
-
-    // this.xr = 6 + 10 * Math.random();
-    // this.yr = 2 + 10 * Math.random();
-    this.r = 85 + 85 * Math.random()
-
-    this.color = colorPallete[Math.floor(Math.random() * colorPallete.length)];
-  }
-
-  update() {
-    this.x += this.vx;
-    this.y += this.vy;
-
-    // this.xr-= .01;
-    // this.yr -= .01;
-    // this.r=Math.min(this.yr,this.xr);
-    this.r -= .01;
-
-  }
+class Red {
+	constructor() {
+		this.x = src.x * 2  +350;
+		this.y = (src.y / 2) + src.y + ((Math.random()-0.5)*src.y);
+		this.angle = Math.PI + ((Math.random()-0.5)*0.3);
+		var speed=1.2 + Math.random()*2.5;
+		this.vx = speed* Math.cos(this.angle);
+		this.vy = speed* Math.sin(this.angle);
+		
+		// this.xr = 6 + 10 * Math.random();
+		// this.yr = 2 + 10 * Math.random();
+		this.r = 85 + 85 * Math.random()
+		
+		this.color = colorPallete[Math.floor(Math.random() * redPallete.length)];
+	}
+	
+	update() {
+		this.x += this.vx;
+		this.y += this.vy;
+		
+		// this.xr-= .01;
+		// this.yr -= .01;
+		// this.r=Math.min(this.yr,this.xr);
+		this.r -= .01;
+		
+	}
 }
 
 class Blue {
-  constructor() {
-    this.x = (src.x * 2) + 100;
-    this.y = src.y / 2 + ((Math.random()-0.5)*src.y);
-    // this.angle = Math.PI * 2 * Math.random();
-    this.angle = Math.PI + (Math.random()-0.5)*0.2;
-    var speed=0.8 + Math.random()*1.2;
-    this.vx = speed* Math.cos(this.angle);
-    this.vy = speed* Math.sin(this.angle);
-
-    // this.xr = 6 + 10 * Math.random();
-    // this.yr = 2 + 10 * Math.random();
-    this.r = 60 + 85 * Math.random()
-
-    this.color = bluePallete[Math.floor(Math.random() * colorPallete.length)];
-  }
-
-  update() {
-    this.x += this.vx;
-    this.y += this.vy;
-
-    // this.xr-= .01;
-    // this.yr -= .01;
-    // this.r=Math.min(this.yr,this.xr);
-    this.r -= .01;
-
-  }
+	constructor() {
+		this.x = (src.x * 2) + 350;
+		this.y = src.y / 2 + ((Math.random()-0.5)*src.y);
+		// this.angle = Math.PI * 2 * Math.random();
+		this.angle = Math.PI + (Math.random()-0.5)*0.2;
+		var speed=1.2 + Math.random()*3.5;
+		this.vx = speed* Math.cos(this.angle);
+		this.vy = speed* Math.sin(this.angle);
+		
+		// this.xr = 6 + 10 * Math.random();
+		// this.yr = 2 + 10 * Math.random();
+		this.r = 80 + 85 * Math.random()
+		
+		this.color = bluePallete[Math.floor(Math.random() * bluePallete.length)];
+	}
+	
+	update() {
+		this.x += this.vx;
+		this.y += this.vy;
+		
+		// this.xr-= .01;
+		// this.yr -= .01;
+		// this.r=Math.min(this.yr,this.xr);
+		this.r -= .01;
+		
+	}
 }
 
 function removeCircles() {
- circles = circles.filter(
-    (b) =>
-      !(
-        b.x + b.r < 0 ||
-        b.x - b.r > width+200 ||
-        b.y + b.r < 0 ||
-        b.y - b.r > height ||
-        b.r < 0
-      )
-  );
-  blues = blues.filter(
-     (b) =>
-       !(
-         b.x + b.r < 0 ||
-         b.x - b.r > width+200 ||
-         b.y + b.r < 0 ||
-         b.y - b.r > height ||
-         b.r < 0
-       )
-   );
+	reds = reds.filter(
+		(b) =>
+		!(
+			b.x + b.r < 0 ||
+			b.x - b.r > width+400 ||
+			b.y + b.r < 0 ||
+			b.y - b.r > height ||
+			b.r < 0
+		)
+	);
+	blues = blues.filter(
+		(b) =>
+		!(
+			b.x + b.r < 0 ||
+			b.x - b.r > width+400 ||
+			b.y + b.r < 0 ||
+			b.y - b.r > height ||
+			b.r < 0
+		)
+	);
 }
 
+var bar_height = 0;
 function renderCircles() {
-  context.clearRect(0, 0, width, height);
-
-  if (Math.random() > 0.97){
-    // circles.push(new Circle());
-    blues.push(new Blue());
-  }
-
-  for (var i = 0; i < circles.length; i++) {
-    var b = circles[i];
-    context.fillStyle = b.color;
-    context.beginPath();
-
-    context.arc(b.x, b.y, b.r, 0, Math.PI * 2, false);
-    // context.ellipse(b.x, b.y, b.xr, b.yr, b.angle, 0, 2 * Math.PI);
-
-    context.fill();
-    b.update();
-  }
-  for (var i = 0; i < blues.length; i++) {
-    var b = blues[i];
-    context.fillStyle = b.color;
-    context.beginPath();
-
-    context.arc(b.x, b.y, b.r, 0, Math.PI * 2, false);
-    // context.ellipse(b.x, b.y, b.xr, b.yr, b.angle, 0, 2 * Math.PI);
-
-    context.fill();
-    b.update();
-  }
-  context.fillStyle= colorPallete[0];
-  context.fillRect(0, src.y*3/2, src.x*2, src.y*2);
-  context.fillStyle= bluePallete[0];
-  context.fillRect(0, 0, src.x*2, src.y/2);
-  if (Math.random() > 0.8){
-    let noise=document.getElementById("noise");
-    noise.setAttribute("seed",Math.floor(Math.random()*10));
-  }
-  removeCircles();
-  requestAnimationFrame(renderCircles);
+	context.clearRect(0, 0, width, height);
+	if (Math.random() > 0.97){
+		blues.push(new Blue());
+	}
+	// if (Math.random() > 0.97){
+	// 	reds.push(new Red());
+	// }
+	for (var i = 0; i < reds.length; i++) {
+		var r = reds[i];
+		context.fillStyle = r.color;
+		context.beginPath();
+		
+		context.arc(r.x, r.y, r.r, 0, Math.PI * 2, false);
+		// context.ellipse(b.x, b.y, b.xr, b.yr, b.angle, 0, 2 * Math.PI);
+		
+		context.fill();
+		r.update();
+	}
+	for (var i = 0; i < blues.length; i++) {
+		var b = blues[i];
+		context.fillStyle = b.color;
+		context.beginPath();
+		
+		context.arc(b.x, b.y, b.r, 0, Math.PI * 2, false);
+		// context.ellipse(b.x, b.y, b.xr, b.yr, b.angle, 0, 2 * Math.PI);
+		
+		context.fill();
+		b.update();
+	}
+	var t_bar_height;
+	if (bar_height > 400){
+		bar_height = 0;
+	}
+	if (bar_height > 200){
+		t_bar_height = 400 - bar_height;
+	} else {
+		t_bar_height = bar_height;
+	}
+	// context.fillStyle= colorPallete[0];
+	// context.fillRect(0, src.y*3/2, src.x*2, src.y*2);
+	context.fillStyle= bluePallete[0];
+	context.fillRect(0, 0, src.x*2, (src.y/2)-50+t_bar_height/2);
+	if ((bar_height % 8) == 0){
+		let noise=document.getElementById("noise");
+		noise.setAttribute("seed",Math.floor(Math.random()*10));
+	}
+	bar_height++;
+	removeCircles();
+	requestAnimationFrame(renderCircles);
 }
 
 renderCircles();
-
-// https://codepen.io/mnmxmx/pen/VjjvEq
